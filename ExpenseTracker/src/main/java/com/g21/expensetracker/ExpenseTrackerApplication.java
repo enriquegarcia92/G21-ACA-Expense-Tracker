5e9cadb5c0b1f6a0694016de0d2ef20238535e29
Enrique García <54822214+enriquegarcia92@users.noreply.github.com>
2023-07-03 21:39:10 -0600

    Cors configuration

--- ExpenseTracker/src/main/java/com/g21/expensetracker/ExpenseTrackerApplication.java
+++ ExpenseTracker/src/main/java/com/g21/expensetracker/ExpenseTrackerApplication.java
@@ -2,12 +2,42 @@ package com.g21.expensetracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin
 @SpringBootApplication
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApplication.class, args);
+	}
+
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		List<String> Origins=new ArrayList<String>();
+		Origins.add("*");
+		List<String> Methods=new ArrayList<String>();
+		Methods.add("*");
+		List<String> Headers=new ArrayList<String>();
+		Headers.add("*");
+		List<String> Exposed=new ArrayList<String>();
+		Exposed.add("*");
+
+		configuration.setAllowedOrigins(Origins);
+		configuration.setAllowedMethods(Methods);
+		configuration.setAllowedHeaders(Headers);
+		configuration.setExposedHeaders(Exposed);
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 }
